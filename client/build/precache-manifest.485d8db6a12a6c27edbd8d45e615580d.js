113102816e4bce9430dbc8d0185e87fcc9b6e643
anchal12ajay12 <40241644+anchal12ajay12@users.noreply.github.com>
2020-05-15 01:29:25 +0530

    first commit

---
+++ client/build/precache-manifest.485d8db6a12a6c27edbd8d45e615580d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6a705ab33288bbb2aa8eaf37de6d87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b09291d048eae7616c74",
+    "url": "/static/css/main.4f04a7c4.chunk.css"
+  },
+  {
+    "revision": "e274dad7b91ac618e540",
+    "url": "/static/js/2.92373eb4.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.92373eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09291d048eae7616c74",
+    "url": "/static/js/main.5db37c4b.chunk.js"
+  },
+  {
+    "revision": "986dd41c12b747f8195a",
+    "url": "/static/js/runtime-main.13167495.js"
+  }
+]);
